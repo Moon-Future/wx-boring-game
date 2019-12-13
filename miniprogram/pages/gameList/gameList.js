5f7c9d53edc0debd5859a44cdf04c52ce00b44ca
ChenLiang <236338364@qq.com>
2019-12-13 09:31:15 +0800

    Initial Commit

---
+++ miniprogram/pages/gameList/gameList.js
@@ -0,0 +1,70 @@
+// miniprogram/pages/gameList/gameList.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    gameList: [
+      {name: '圈小猫', url: '../../pages/catchTheCat/catchTheCat'},
+      {name: 'Google跳跳跳', url: ''},
+      {name: '魔方转转转', url: ''}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
